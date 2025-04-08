af11a759ad1f4eae6b7ec81573fc6e89c5a9008a
chosihung <chosihung@github.com>
2025-04-08 17:18:08 +0900

    처음 커밋

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+print("hello, python + VS Code!")
+print("판다스 버전:", pd.__version__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("NumPy 버전:", np.__version__)
+print("Matplotlib 테스트!")
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title("Sine Wave Test")
+plt.xlabel("x-axis")
+plt.ylabel("y = sin(x)")
+plt.grid()
+plt.show()
